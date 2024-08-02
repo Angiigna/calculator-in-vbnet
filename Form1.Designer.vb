e1344111c8b87965102d9e05a01b6ab0745df974
Sivangi <sivangisankar19@gmail.com>
2024-08-02 09:34:59 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Buttonplus = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Buttonc = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Buttonequal = New System.Windows.Forms.Button()
+        Me.Buttonminus = New System.Windows.Forms.Button()
+        Me.Buttonmultiply = New System.Windows.Forms.Button()
+        Me.Buttondivide = New System.Windows.Forms.Button()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(-6, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 103)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(98, 87)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(105, 103)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(200, 87)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(107, 103)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Buttonplus
+        '
+        Me.Buttonplus.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonplus.Location = New System.Drawing.Point(301, 87)
+        Me.Buttonplus.Name = "Buttonplus"
+        Me.Buttonplus.Size = New System.Drawing.Size(97, 103)
+        Me.Buttonplus.TabIndex = 3
+        Me.Buttonplus.Text = "+"
+        Me.Buttonplus.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(-6, 187)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(109, 103)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(-6, 284)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(109, 103)
+        Me.Button7.TabIndex = 5
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.Location = New System.Drawing.Point(-6, 383)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(109, 103)
+        Me.Button0.TabIndex = 6
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(98, 187)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(105, 103)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(98, 284)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(105, 103)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Buttonc
+        '
+        Me.Buttonc.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonc.Location = New System.Drawing.Point(98, 383)
+        Me.Buttonc.Name = "Buttonc"
+        Me.Buttonc.Size = New System.Drawing.Size(105, 103)
+        Me.Buttonc.TabIndex = 9
+        Me.Buttonc.Text = "C"
+        Me.Buttonc.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button16.Location = New System.Drawing.Point(200, 187)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(107, 103)
+        Me.Button16.TabIndex = 10
+        Me.Button16.Text = "6"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(200, 284)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(107, 103)
+        Me.Button9.TabIndex = 11
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Buttonequal
+        '
+        Me.Buttonequal.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonequal.Location = New System.Drawing.Point(200, 383)
+        Me.Buttonequal.Name = "Buttonequal"
+        Me.Buttonequal.Size = New System.Drawing.Size(107, 103)
+        Me.Buttonequal.TabIndex = 12
+        Me.Buttonequal.Text = "="
+        Me.Buttonequal.UseVisualStyleBackColor = True
+        '
+        'Buttonminus
+        '
+        Me.Buttonminus.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonminus.Location = New System.Drawing.Point(301, 187)
+        Me.Buttonminus.Name = "Buttonminus"
+        Me.Buttonminus.Size = New System.Drawing.Size(97, 103)
+        Me.Buttonminus.TabIndex = 13
+        Me.Buttonminus.Text = "-"
+        Me.Buttonminus.UseVisualStyleBackColor = True
+        '
+        'Buttonmultiply
+        '
+        Me.Buttonmultiply.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttonmultiply.Location = New System.Drawing.Point(301, 284)
+        Me.Buttonmultiply.Name = "Buttonmultiply"
+        Me.Buttonmultiply.Size = New System.Drawing.Size(97, 103)
+        Me.Buttonmultiply.TabIndex = 14
+        Me.Buttonmultiply.Text = "*"
+        Me.Buttonmultiply.UseVisualStyleBackColor = True
+        '
+        'Buttondivide
+        '
+        Me.Buttondivide.Font = New System.Drawing.Font("Microsoft YaHei UI", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Buttondivide.Location = New System.Drawing.Point(301, 383)
+        Me.Buttondivide.Name = "Buttondivide"
+        Me.Buttondivide.Size = New System.Drawing.Size(97, 103)
+        Me.Buttondivide.TabIndex = 15
+        Me.Buttondivide.Text = "/"
+        Me.Buttondivide.UseVisualStyleBackColor = True
+        '
+        'TextBox
+        '
+        Me.TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox.Location = New System.Drawing.Point(-6, 0)
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(404, 89)
+        Me.TextBox.TabIndex = 16
+        Me.TextBox.Text = "0"
+        Me.TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 485)
+        Me.Controls.Add(Me.TextBox)
+        Me.Controls.Add(Me.Buttondivide)
+        Me.Controls.Add(Me.Buttonmultiply)
+        Me.Controls.Add(Me.Buttonminus)
+        Me.Controls.Add(Me.Buttonequal)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Buttonc)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Buttonplus)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Buttonplus As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Buttonc As System.Windows.Forms.Button
+    Friend WithEvents Button16 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Buttonequal As System.Windows.Forms.Button
+    Friend WithEvents Buttonminus As System.Windows.Forms.Button
+    Friend WithEvents Buttonmultiply As System.Windows.Forms.Button
+    Friend WithEvents Buttondivide As System.Windows.Forms.Button
+    Friend WithEvents TextBox As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,148 @@
+﻿Public Class Form1
+    Dim number1 As Decimal
+    Dim number2 As Decimal
+    Dim solution As Decimal
+    Dim operate As String
+    Dim nooperation As Boolean = True
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles TextBox.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "1"
+            Else
+                TextBox.Text = "1"
+            End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "2"
+            Else
+                TextBox.Text = "2"
+            End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "3"
+            Else
+                TextBox.Text = "3"
+            End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "4"
+            Else
+                TextBox.Text = "4"
+            End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "5"
+            Else
+                TextBox.Text = "5"
+            End If
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "6"
+            Else
+                TextBox.Text = "6"
+            End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "7"
+            Else
+                TextBox.Text = "7"
+            End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "8"
+            Else
+                TextBox.Text = "8"
+            End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "9"
+            Else
+                TextBox.Text = "9"
+        End If
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+            If TextBox.Text <> "0" Then
+                TextBox.Text += "0"
+            Else
+                TextBox.Text = "0"
+        End If
+    End Sub
+
+    Private Sub Buttonc_Click(sender As Object, e As EventArgs) Handles Buttonc.Click
+        TextBox.Text = "0"
+    End Sub
+
+    Private Sub Buttonplus_Click(sender As Object, e As EventArgs) Handles Buttonplus.Click
+        number1 = (TextBox.Text)
+        TextBox.Text = "0"
+        nooperation = False
+        operate = "+"
+    End Sub
+
+    Private Sub Buttonequal_Click(sender As Object, e As EventArgs) Handles Buttonequal.Click
+        If nooperation = False Then
+            number2 = (TextBox.Text)
+            If operate = "+" Then
+                solution = number1 + number2
+                TextBox.Text = solution
+            ElseIf operate = "-" Then
+                solution = number1 - number2
+                TextBox.Text = solution
+            ElseIf operate = "*" Then
+                solution = number1 * number2
+                TextBox.Text = solution
+            ElseIf operate = "/" Then
+                solution = number1 / number2
+                TextBox.Text = solution
+            End If
+        End If
+        nooperation = True
+
+    End Sub
+
+    Private Sub Buttonminus_Click(sender As Object, e As EventArgs) Handles Buttonminus.Click
+        number1 = (TextBox.Text)
+        TextBox.Text = "0"
+        nooperation = False
+        operate = "-"
+    End Sub
+
+    Private Sub Buttonmultiply_Click(sender As Object, e As EventArgs) Handles Buttonmultiply.Click
+        number1 = (TextBox.Text)
+        TextBox.Text = "0"
+        nooperation = False
+        operate = "*"
+    End Sub
+
+    Private Sub Buttondivide_Click(sender As Object, e As EventArgs) Handles Buttondivide.Click
+        number1 = (TextBox.Text)
+        TextBox.Text = "0"
+        nooperation = False
+        operate = "/"
+    End Sub
+End Class
